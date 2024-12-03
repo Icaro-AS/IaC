51e225edbac41aecf36bca8a0eb8ef627b3c4c8e
Ícaro Alencar Sousa <ias_epf@hotmail.com>
2024-12-03 14:58:01 -0300

    Alteração para criar uma instância ec2, criar um arquivo html simples e comandos bash para servir esse arquivo.

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
   key_name = "chave-iac"
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                echo "<h1>Deploy com Terraform</h1>" > index.html
+                nohup busybox httpd -f -p 8080 & 
+                EOF
   tags = {
-    Name = "tech-service-instance"
+    Name = "tech-service-instance-teste"
   }
 }
